59f76dd1100886a3a5c9bc9ce07219bab9ff3d45
inputx <inputx@goodays.com>
2014-10-24 21:27:39 +1100

    ### Setup Structure `structure`
    > setup structure for library
    
     - asset_config.php: configurations
     - asset_helper.php: asset output helper for views
     - asset.php: asset library
    
    ---

---
+++ application/helpers/asset_helper.php
@@ -0,0 +1,29 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * 	CodeIgniter Asset Library
+ * 	An open source PHP library written to easily manage assets for CodeIgniter Framework
+ *  
+ *  Copyright 2014  Daniel Lee
+ *	GitHub:  https://github.com/inputx/CodeIgniter-Asset-Library
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the Apache License (2.0) as published by
+ *  the The Apache Software Foundation, either version 2.0 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  Apache License for more details.
+ *
+ *  You should have received a copy of the Apache License
+ *  along with this program.  If not, see <http://www.apache.org/licenses/>
+ *
+ * @package			dli/framework-library/codeIgniter-asset-library
+ * @author			Daniel Lee
+ * @copyright		Copyright 2014 Daniel Lee
+ * @link			https://github.com/inputx/CodeIgniter-Asset-Library
+ * @since			Version 0.1
+ * @updated			24th Oct 2014
+ * @filesource
+*/
